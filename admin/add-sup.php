159c9c721076b42ecfd32c05290936f8f4a5d72f
Lakmal98 <lakmalepp@gmail.com>
2020-01-26 13:37:40 +0530

    Supplier add finished

--- admin/add-sup.php
+++ admin/add-sup.php
@@ -1,19 +1,36 @@
 <?php require_once("include/top.php"); ?>
 
+<?php 
+	if (isset($_POST['submit'])) {
+		$sql = "SELECT * FROM supplier WHERE name = '{$_POST['sName']}' OR nicORbrn = '{$_POST['nic']}';";
+		$query = $conn->query($sql);
+		if ($query->num_rows >= 1) {
+			echo "<script>alert('Supplier already registerd.');</script>";
+		} else {
+			$sql = "INSERT INTO supplier (name, Address, nicORbrn) VALUES ('{$_POST['sName']}', '{$_POST['address']}','{$_POST['nic']}');";
+			if ($conn->query($sql) === true) {
+				echo "<script>alert('Supplier added successfully');</script>";
+			} else {
+				echo "<script>alert('Failed to add supplier.');</script>";
+			}
+		}
+	}
+ ?>
+
 			<div class="content">
 				<h1 align="center">Add Supplier</h1>
 				<form name="add_supplier" method="post" action="<?php echo chop($_SERVER['PHP_SELF'], '.php') . 'p'; ?>">
 				    <label for="SName">Supplier Name</label>
-				    <input type="text" name="Sname" >
+				    <input type="text" name="sName" id="sName">
 
 					<label for="address">Address</label>
 				    <input type="text" name="address" >
 
-					<label for="nic">NIC No</label>
+					<label for="nic">NIC or BR No</label>
 				    <input type="text" name="nic" >
 
 				    <button class="btn" type="submit" name="submit"> Add Suplier</button>
 				</form>
 			</div>
-
+<script src="js/add-sup.js"></script>
 <?php require_once("include/bottom.php"); ?>
---
+++ admin/js/add-sup.js
@@ -0,0 +1,4 @@
+window.onload = function() {
+	_('sName').focus();
+} 
+
